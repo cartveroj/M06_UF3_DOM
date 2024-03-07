25fc90e0e2c67b2b74cc9bc92009905088e8503f
cartveroj <cartagenaveronica.19@gmail.com>
2024-03-07 18:57:09 +0100

    avance 07.03.24

--- GoogleMaps/js/script.js
+++ GoogleMaps/js/script.js
@@ -2,60 +2,104 @@
 let buscar = document.getElementById("findLoc");
 let Ilatitude = document.getElementById("latitude");
 let Ilongitude = document.getElementById("longitude");
+let btn = document.getElementById("geo");
+let btnInit = document.getElementById("initMap");
+let mensaje = document.getElementById("mensaje");
+
+let map;
+//funcion que inicia el mapa 
+async function initMap() {
+  const { Map } = await google.maps.importLibrary("maps");
+  let deflat= 41.390205;
+  let deflng= 2.154007;
+  const myLatLng = { lat: deflat, lng: deflng };
+  map = new Map(document.getElementById("map"), {
+    center: myLatLng,
+    zoom: 12,
+  });
+
+  addMarker(myLatLng);
+}
+initMap();
+
 buscar.addEventListener('click', function(){
+        Ilatitude.value = " ";
+        Ilongitude.value = " ";
+        let p = document.getElementById('text');
+        if(p != null){
+         mensaje.removeChild(p);
+        }
+       
         let inputUser= document.getElementById('adreca').value;
         console.log(inputUser);
 
         let geocoder = new google.maps.Geocoder();
         let address=`"${inputUser}"`;
-"Carrer de la Selva de Mar 211 08020 Barcelona"
+        console.log()
         geocoder.geocode( { 'address': address}, function(results, status) {
 
             if (status == google.maps.GeocoderStatus.OK) {
-            latitude = results[0].geometry.location.lat();
-            console.log(latitude);
-            Ilatitude.value = latitude;
-            console.log(Ilatitude);
-            longitude = results[0].geometry.location.lng();
-            Ilongitude.value = latitude;
-            console.log(typeof(longitude));
-            console.log(Ilongitude);
+             
+              latitude = results[0].geometry.location.lat();
+              Ilatitude.value = latitude;
+              longitude = results[0].geometry.location.lng();
+              Ilongitude.value = longitude;
+              ubicacionEnMaps(latitude,longitude);
+
+            }else{
+
+              let p = document.createElement('p');
+              p.id = "text";
+              let text = document.createTextNode("La dirección introducida no es válida");
+              p.appendChild(text);
+              mensaje.appendChild(p);
             }
         });
     });
  
-let map;
 
-async function initMap() {
+async function ubicacionEnMaps(latitude,longitude) {
   const { Map } = await google.maps.importLibrary("maps");
-    let 
+  const myLatLng = { lat: latitude, lng: longitude };
   map = new Map(document.getElementById("map"), {
-    center: { lat: 41.390205, lng: 2.154007 },
-    zoom: 16,
+    center: myLatLng,
+    zoom: 12,
   });
-
-  google.maps.Marker({
-    position: center,
-    map,
-    title: "Hello World!",
-  });
+  
+  addMarker(myLatLng);
+  
 }
 
-initMap();
+btn.addEventListener('click',geolocalizacion);
 
-async function initMap() {
-    const myLatLng = { lat: Ilatitude.value, lng: Ilongitude.value };
-    const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 16,
-      center: myLatLng,
-    });
-  
-    new google.maps.Marker({
-      position: myLatLng,
-      map,
-      title: "Hello World!",
+function geolocalizacion(){
+  const image = {
+    url:'img/persona.png',
+    scaledSize: new google.maps.Size(30, 42),
+  }
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      let pos = {
+      lat: position.coords.latitude,
+      lng: position.coords.longitude
+      };
+      map.setCenter(pos);
+      map.setZoom(16);
+      new google.maps.Marker({
+        position: pos,
+        map: map,
+        icon: image,
+      });
     });
   }
-  
-window.initMap = initMap;
+}
+
+btnInit.addEventListener('click',initMap);
+
+function addMarker(position){
+  const label = new google.maps.Marker({
+    position,
+    map,
+  });
+}
 
